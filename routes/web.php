a70cebef3094a2e69be88ea5b0d14df5093ada4d
Ney Gelbcke Junior <juniorgelbcke@gmail.com>
2022-07-01 17:50:51 -0300

    refactore_routes

--- routes/web.php
+++ routes/web.php
@@ -15,57 +15,104 @@ use Illuminate\Support\Facades\Redirect;
 |
 */
 
+/**
+ * FronEnd
+ */
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::post('/login/locked', [App\Http\Controllers\Auth\LoginController::class, 'unlock'])->name('login.unlock');
+/**
+ * Unlock System
+ */
+Route::post('/login/locked', [App\Http\Controllers\Auth\LoginController::class, 'unlock'])
+    ->name('login.unlock');
 
+/**
+ * Backend Routes
+ */
 Route::group(['middleware' => ['auth', 'check_user_status', 'check_user_single_session', 'auth.lock', '2fa', 'language']], function () {
-    Route::get('/home', [App\Http\Controllers\Backend\HomeController::class, 'index'])->name('home');
 
-    Route::get('/login/locked', [App\Http\Controllers\Auth\LoginController::class, 'locked'])->withoutMiddleware('auth.lock')->name('login.locked');
+    /**
+     * Dashboard / Home
+     */
+    Route::get('/home', [App\Http\Controllers\Backend\HomeController::class, 'index'])
+        ->name('home');
 
-    Route::get('/lang/{language}', [App\Http\Controllers\Backend\UserController::class, 'change_language'])->name('lang.update');
+    /**
+     * Lockscreen
+     */
+    Route::get('/login/locked', [App\Http\Controllers\Auth\LoginController::class, 'locked'])
+        ->withoutMiddleware('auth.lock')->name('login.locked');
 
-    Route::get('/users', [App\Http\Controllers\Backend\UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [App\Http\Controllers\Backend\UserController::class, 'create'])->name('users.create');
-    Route::get('/users/edit/{user}', [App\Http\Controllers\Backend\UserController::class, 'edit'])->name('users.edit');
-    Route::post('/users/store', [App\Http\Controllers\Backend\UserController::class, 'store'])->name('users.store');
-    Route::patch('/users/update/{user}', [App\Http\Controllers\Backend\UserController::class, 'update'])->name('users.update');
-    Route::get('/users/getUsers', [App\Http\Controllers\Backend\UserController::class, 'getUsers'])->name('users.get');
-    Route::get('/users/lockscreen', [App\Http\Controllers\Backend\UserController::class, 'lockscreen'])->name('users.lockscreen');
+    /**
+     * User / Profile
+     */
+    Route::controller(App\Http\Controllers\Backend\UserController::class)->group(function () {
+        Route::prefix('users')->group(function () {
+            Route::get('/', 'index')->name('users.index');
+            Route::get('/create', 'create')->name('users.create');
+            Route::get('/edit/{user}', 'edit')->name('users.edit');
+            Route::post('/store', 'store')->name('users.store');
+            Route::patch('/update/{user}', 'update')->name('users.update');
+            Route::get('/getUsers', 'getUsers')->name('users.get');
+            Route::get('/{language}', 'change_language')->name('lang.update');
+            Route::get('/lockscreen', 'lockscreen')->name('users.lockscreen');
+            Route::get('/activate/{id}', 'active')->name('users.active');
+            Route::get('/deactivate/{id}', 'deactive')->name('users.deactive');
+        });
+        Route::prefix('profile')->group(function () {
+            Route::get('/{user}', 'user_profile')->name('user.profile');
+            Route::get('/my_profile/{user}', 'profile')->name('profile');
+            Route::patch('/{user}', 'update_profile')->name('profile.update');
+        });
+    });
 
-    Route::get('/users_a/{id}', [App\Http\Controllers\Backend\UserController::class, 'active'])->name('users.active');
-    Route::get('/users_d/{id}', [App\Http\Controllers\Backend\UserController::class, 'deactive'])->name('users.deactive');
+    /**
+     * System Settings
+     */
+    Route::controller(App\Http\Controllers\Backend\SystemSettingController::class)->group(function () {
+        Route::prefix('system')->group(function () {
+            Route::get('/settings', 'index')->name('system_settings');
+            Route::get('/settings/edit', 'settings')->name('system_settings.edit');
+            Route::post('/settings/edit', 'update')->name('system_settings.update');
+        });
+    });
 
-    Route::get('/user/profile/{user}', [App\Http\Controllers\Backend\UserController::class, 'user_profile'])->name('user.profile');
-    Route::get('/profile/{user}', [App\Http\Controllers\Backend\UserController::class, 'profile'])->name('profile');
-    Route::post('/profile/{user}', [App\Http\Controllers\Backend\UserController::class, 'update_profile'])->name('profile.update');
+    /**
+     * Logs
+     */
+    Route::controller(App\Http\Controllers\Backend\LogController::class)->group(function () {
+        Route::prefix('log')->group(function () {
+            Route::get('/', 'index')->name('logs.index');
+            Route::get('/getLogs', 'getLogs')->name('logs.get');
+            Route::get('/clearLogs', 'clearLogs')->name('logs.clear');
+        });
+    });
 
-    Route::get('/system/settings', [App\Http\Controllers\Backend\SystemSettingController::class, 'index'])->name('system_settings');
-    Route::get('/system/settings/edit', [App\Http\Controllers\Backend\SystemSettingController::class, 'settings'])->name('system_settings.edit');
-    Route::post('/system/settings/edit', [App\Http\Controllers\Backend\SystemSettingController::class, 'update'])->name('system_settings.update');
+    /**
+     * 2FA
+     */
+    Route::controller(App\Http\Controllers\Backend\LoginSecurityController::class)->group(function () {
+        Route::prefix('2fa')->group(function () {
+            Route::any('/', 'show2faForm')->name('2fa');
+            Route::post('/secret',  'generate2faSecret')->name('generate2faSecret');
+            Route::post('/enable', 'enable2fa')->name('enable2fa');
+            Route::post('/disable', 'disable2fa')->name('disable2fa');
+        });
+    });
 
-    Route::get('/log', [App\Http\Controllers\Backend\LogController::class, 'index'])->name('logs.index');
-    Route::get('/log/getLogs', [App\Http\Controllers\Backend\LogController::class, 'getLogs'])->name('logs.get');
-    Route::get('/log/clearLogs', [App\Http\Controllers\Backend\LogController::class, 'clearLogs'])->name('logs.clear');
-
-    Route::any('/2fa', [App\Http\Controllers\Backend\LoginSecurityController::class, 'show2faForm'])->name('2fa');
-    Route::post('/2fa/secret', [App\Http\Controllers\Backend\LoginSecurityController::class, 'generate2faSecret'])->name('generate2faSecret');
-    Route::post('/2fa/enable', [App\Http\Controllers\Backend\LoginSecurityController::class, 'enable2fa'])->name('enable2fa');
-    Route::post('/2fa/disable', [App\Http\Controllers\Backend\LoginSecurityController::class, 'disable2fa'])->name('disable2fa');
-
-
-
+    /**
+     * Roles and permissions
+     */
     Route::resource('/roles', App\Http\Controllers\RoleController::class);
     Route::resource('/permissions', App\Http\Controllers\PermissionController::class);
 
-
-
-    // 2fa middleware
+    /**
+     * 2FA middleware
+     */
     Route::post('/2faVerify', function () {
         return redirect(URL()->previous());
     })->name('2faVerify');
